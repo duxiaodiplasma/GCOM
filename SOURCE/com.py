Correct the normalization

SOURCE/com.py
@@ -166,16 +166,21 @@ def orbit_check(ob_vpara,obr,obz,rbbbs,zbbbs,rmaxis,f_psi,sibry,simag):
     # Conf. -  counter-Ip Passing  5
     # Conf. -  co-Ip Passing       6
 
-#    outboundary =  \
-#    [obr>np.max(rbbbs), obr<np.min(rbbbs), \
-#     obz>np.max(zbbbs), obz<np.min(zbbbs)]
+    # 20170418
+    # this is demonstrated to be not enough in some rare case,
+    # stronger constrain is developped
+    #outboundary =  \
+    #[obr>np.max(rbbbs), obr<np.min(rbbbs), \
+    # obz>np.max(zbbbs), obz<np.min(zbbbs)]
+
+    # 20170418
     rho = trace.RZ_to_RHO(obr,obz,f_psi,simag,sibry)
     if np.max(np.abs(rho)) > 1:
        outboundary = True
     else:
        outboundary = False
 
-    if outboundary: \
+    if outboundary:
        # hit the wall, lost particles
        if np.any(np.sign(ob_vpara) ==1) and np.any(np.sign(ob_vpara) ==-1):
           # lost, trapped particles

SOURCE/trace.py
@@ -36,9 +36,9 @@ def main(
     ob_contour \
     = com.f_mucontour(RR,ZZ,murz,vpararz,rbbbs,zbbbs,mu0,rmaxis,R0,Z0,nseg,f_psi,sibry,simag)
 
-    #pphi = pphi0/(simag-sibry)/gl.q
-    pphi = pphi0/sibry/gl.q
-    #pphi = pphi0/gl.q
+    # normalized pphi
+    pphi = (pphi0/gl.q-simag)/(sibry-simag)
+    # normalized mu
     mu_E = mu0*np.abs(bc)/(E0/1e3/gl.q)
     ob   = ob_contour['orbit_class']
     fvpara = ob_contour['fvpara']